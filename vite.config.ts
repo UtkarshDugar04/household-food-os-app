13b82f806cf16f22f8a0c506a267422913a1456b
Utkarsh Dugar <utkarshdugar@Utkarshs-MacBook-Air.local>
2026-06-18 01:19:34 +0530

    Update links for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/figma-makeathon-design-system/',
+  base: '/household-food-os-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
